254fe2ec4d4181eb139d697b67cfb2b9197f5845
github username <github email address>
2019-01-14 15:56:16 -0600

    login frame set

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { CreateStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+// import { Provider } from "react-redux";
+// import { CreateStore, compose, applyMiddleware } from "redux";
+// import thunk from "redux-thunk";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
