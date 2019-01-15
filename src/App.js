3153fdad95e0193389ed07c4a47dbc6b6ca940b3
github username <github email address>
2019-01-15 00:02:46 -0600

    rating component and form done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./Components/Login";
 // import { Switch, Route, Redirect } from "react-router";
 import AppBar from "./Components/AppBar";
 import Ratings from "./Components/Ratings";
+import RatingsForm from "./Components/RatingsForm";
 
 const API_KEY = `${process.env.REACT_APP_MOVIE_API_KEY}`;
 
@@ -15,6 +16,8 @@ class App extends Component {
         <AppBar />
         <Login />
         <Ratings />
+        <hr />
+        <RatingsForm />
 
         {/* <Router>
           <Switch>
--- src/Components/Ratings.js
+++ src/Components/Ratings.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
 
-export default class Rating extends Component {
+export default class Ratings extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ratings: []
+    };
+  }
+
+  ComponentWillMount() {
+    fetch("http://localhost:3000/api/v1/ratings")
+      .then(res => res.json())
+      .then(data => this.setState({ ratings: data }));
+  }
   render() {
+    const ratingItems = this.state.ratings.map(rating => (
+      <div key={rating.id}>
+        <h3>{rating.score}</h3>
+        <p>{rating.comment}</p>
+      </div>
+    ));
     return (
       <div>
         <h1>Ratings</h1>
